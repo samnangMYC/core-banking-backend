7f949cab60ae5f6ae18a5451dadad60b6242a0aa
samnang <samnangproducer@gmail.com>
2025-11-21 14:51:55 +0700

    first

---
+++ src/main/java/com/trendy/cbs/CoreBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trendy.cbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBankingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreBankingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trendy/cbs/CoreBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendy.cbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
